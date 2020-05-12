cda58611b7a28b25349824c4dabcb9248e01631b
Rodrigo Drumond de Paula <rodrigodru33@gmail.com>
2020-05-11 22:00:23 -0300

    init seleniun repository

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/rdrumond/project/begin-selenium-python/venv"
+set -gx VIRTUAL_ENV "/home/rdrumond/project/starting-selenium-python/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
